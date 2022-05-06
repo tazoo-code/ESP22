93210050fcb400eb74c42e17408937989d71c4fd
MarcoVettore <marco.vettore.9@studenti.unipd.it>
2022-05-06 15:16:57 +0200

    Aggiunto le dipendenze di ArCore e jpct_ae

--- app/src/main/java/com/example/esp22/MainActivity.kt
+++ app/src/main/java/com/example/esp22/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.esp22
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.ar.core.ArCoreApk
+import com.threed.jpct.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Check della disponibilità
+        val availability = ArCoreApk.getInstance().checkAvailability(this)
+        if(!availability.isSupported){
+            //TODO agire in qualche modo quando l'applicazione non supporta ArCore
+        }
+
         setContentView(R.layout.activity_main)
     }
 }
