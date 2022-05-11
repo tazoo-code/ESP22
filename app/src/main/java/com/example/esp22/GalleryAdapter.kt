f6e63115cb2c90d789fd8ef6d49eeb4fc356db81
tazoo <76154837+tazoo-code@users.noreply.github.com>
2022-05-11 09:28:57 +0200

    Grafica slider

--- app/src/main/java/com/example/esp22/GalleryAdapter.kt
+++ app/src/main/java/com/example/esp22/GalleryAdapter.kt
@@ -1,20 +1,30 @@
 package com.example.esp22
 
-import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import javax.xml.datatype.DatatypeConstants
 
 class GalleryAdapter(private val list: Array<String>) :
     RecyclerView.Adapter<GalleryAdapter.ItemViewHolder>() {
+
+    private val onClickListener = View.OnClickListener { v ->
+
+        Toast.makeText(v.context, "Premuto", Toast.LENGTH_SHORT).show()
+        /*val flowerName = v.findViewById<TextView>(R.id.flower_text).text
+        val intent = Intent(v.context, DetailActivity::class.java)
+        intent.putExtra(DetailActivity.ARG_FLOWER_NAME, flowerName)
+        v.context.startActivity(intent)*/
+    }
 
     // Ritorna un nuovo ViewHolder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.gallery_item, parent, false)
-
+        view.setOnClickListener(onClickListener)
         return ItemViewHolder(view)
     }
 
