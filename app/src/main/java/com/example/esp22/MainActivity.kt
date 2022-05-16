5e391a23174ccb04de98c28884396722f9301f9b
Alberto <albertovarini.av@gmail.com>
2022-05-16 14:29:33 +0200

    Piazzamento oggetti: cubo,spada funzionanti

--- app/src/main/java/com/example/esp22/MainActivity.kt
+++ app/src/main/java/com/example/esp22/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.example.esp22
 
-import android.content.ContentResolver
-import android.content.res.Resources
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
@@ -14,7 +12,6 @@ import com.google.ar.sceneform.rendering.Renderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.TransformableNode
 
-
 class MainActivity : AppCompatActivity() {
 
     lateinit var arFragment: ArFragment
@@ -25,25 +22,7 @@ class MainActivity : AppCompatActivity() {
     private var model: Renderable? = null
     var cubeRenderable: ModelRenderable? = null
 
-    /*private val onTapPlane = arFragment.setOnTapArPlaneListener { hitResult,plane, motionEvent ->
-        arFragment.arSceneView.scene.addChild(AnchorNode(hitResult.createAnchor()).apply {
-            // Create the transformable model and add it to the anchor.
-            addChild(TransformableNode(arFragment.transformationSystem).apply {
-                val model = createModel(1)
-
-                renderable = cubeRenderable
-                //renderableInstance.animate(true).start()
-                // Add child model relative the a parent model
-                addChild(Node().apply {
-                    // Define the relative position
-                    localPosition = Vector3(0.0f, 1f, 0.0f)
-                    // Define the relative scale
-                    localScale = Vector3(0.7f, 0.7f, 0.7f)
-                    //renderable = modelView
-                })
-            })
-        })
-    }*/
+    //lateinit var object
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,24 +31,28 @@ class MainActivity : AppCompatActivity() {
         arFragment = (supportFragmentManager.findFragmentById(R.id.arFragment) as ArFragment)
 
 
-        /*arFragment.setOnTapArPlaneListener { hitResult, plane, motionEvent ->
+        arFragment.setOnTapArPlaneListener { hitResult,plane, motionEvent ->
+
+            /*
+            setModel()
 
             val anchor = hitResult.createAnchor()
             val anchorNode = AnchorNode(anchor)
-            anchorNode.setParent(arFragment.arSceneView.scene)
-            createModel(anchorNode, 1)*/
+            val tn = TransformableNode(arFragment.transformationSystem)
+            tn.parent = anchorNode
+            tn.renderable = cubeRenderable
+            tn.select()
 
-            arFragment.setOnTapArPlaneListener { hitResult,plane, motionEvent ->
-
+            */
             arFragment.arSceneView.scene.addChild(AnchorNode(hitResult.createAnchor()).apply {
+
                 setModel()
+
                 // Create the transformable model and add it to the anchor.
                 addChild(TransformableNode(arFragment.transformationSystem).apply {
-                    //val model = createModel(1)
 
                     renderable = cubeRenderable
-                    //renderableInstance.setCulling(false)
-                    //renderableInstance.animate(true).start()
+
                     // Add child model relative the a parent model
                     addChild(Node().apply {
                         // Define the relative position
@@ -82,39 +65,26 @@ class MainActivity : AppCompatActivity() {
             })
         }
 
-        }
+
+    }
 
     private fun setModel() {
 
-        //Modo1
-        //UriParse("/assets/models/cub.gltf")
-
-        //Modo2
-        //R.raw.cub
-
-        //Modo3
-        //UriParse("android.resource://"+PACKAGE_NAME+"/" + R.raw.cub")
-
-        //Modo4
-        val resources: Resources = this.getResources()
-        val uri = Uri.Builder()
-            .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
-            .authority(resources.getResourcePackageName(R.raw.cub))
-            .appendPath(resources.getResourceTypeName(R.raw.cub))
-            .appendPath(resources.getResourceEntryName(R.raw.cub))
-            .build()
-
         ModelRenderable.builder()
-            .setSource(this, uri)
+            .setSource(this, Uri.parse("models/spada.glb") )
             .setIsFilamentGltf(true)
             .build()
             .thenAccept { model: ModelRenderable -> cubeRenderable = model }
             .exceptionally {
-                Toast.makeText(this, "Unable to load Cube model", Toast.LENGTH_SHORT)
+                val t = Toast.makeText(this, "Unable to load Cube model", Toast.LENGTH_SHORT)
+                t.show()
                 null
             }
 
     }
 
 }
+
+
+
 
