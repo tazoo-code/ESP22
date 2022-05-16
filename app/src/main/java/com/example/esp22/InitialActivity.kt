8405503da06811ed822432dc3a6a8c0f704ffdfa
tazoo <76154837+tazoo-code@users.noreply.github.com>
2022-05-16 15:54:52 +0200

    Altre modifiche UI

--- app/src/main/java/com/example/esp22/InitialActivity.kt
+++ app/src/main/java/com/example/esp22/InitialActivity.kt
@@ -2,20 +2,17 @@ package com.example.esp22
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 
 class InitialActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout)
+        setContentView(R.layout.activity_layout)
 
         val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
-        val numberOfColumns = 3
-        recyclerView.layoutManager = GridLayoutManager(this, numberOfColumns)
         // TODO prendere/salvare le preview
-        val list = arrayOf("1", "2", "3", "4", "5")
+        val list = arrayOf("1", "2", "3", "4", "5","1", "2", "3", "4", "5","1", "2", "3", "4", "5","1", "2", "3", "4", "5")
         //Applica l'adapter alla recyclerView
         recyclerView.adapter = GalleryAdapter(list)
 
