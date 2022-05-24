83e9718d623e72b7ac0a8da5b32b499c747c716b
MarcoVettore <marco.vettore.9@studenti.unipd.it>
2022-05-24 16:24:48 +0200

    Prova di spawn di un oggetto(fallita)

--- app/src/main/java/com/example/esp22/AugmentedImagesActivity.kt
+++ app/src/main/java/com/example/esp22/AugmentedImagesActivity.kt
@@ -1,21 +1,30 @@
 package com.example.esp22
 
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.ar.core.*
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.Node
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.BaseArFragment
+import java.util.concurrent.CompletableFuture
 
 
 class AugmentedImagesActivity: AppCompatActivity() {
 
     private lateinit var arFragment: ArFragment
 
-    private var tv1:TextView?=null
+    private var tv1: TextView? = null
+    private var model: CompletableFuture<ModelRenderable>? = null
+    private var centerNode: Node? = null
+    var modelSelected = false
 
-    private lateinit var database:  AugmentedImageDatabase
+    private lateinit var database: AugmentedImageDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -23,7 +32,7 @@ class AugmentedImagesActivity: AppCompatActivity() {
 
         setContentView(R.layout.activity_augmented_images)
 
-        tv1= findViewById<TextView>(R.id.tx1)
+        tv1 = findViewById<TextView>(R.id.tx1)
 
 
         //Riferimento al ArFragment
@@ -52,26 +61,57 @@ class AugmentedImagesActivity: AppCompatActivity() {
             }
 
         }
+        setModel()
     }
 
-    private val onAugmentedImageTrackingUpdate=
-        BaseArFragment.OnAugmentedImageUpdateListener{ augmentedImage ->
-            Log.i("AugmentedImage","Image Found")
-            if(augmentedImage.trackingState == TrackingState.TRACKING
-                && augmentedImage.trackingMethod == AugmentedImage.TrackingMethod.FULL_TRACKING){
-                Log.i("AugmentedImage","Image ${augmentedImage.name} tracked")
-                    when(augmentedImage.name){
+    private val onAugmentedImageTrackingUpdate =
+        BaseArFragment.OnAugmentedImageUpdateListener { augmentedImage ->
+            //Log.i("AugmentedImage", "Image Found")
+            if (augmentedImage.trackingState == TrackingState.TRACKING
+                && augmentedImage.trackingMethod == AugmentedImage.TrackingMethod.FULL_TRACKING
+            ) {
+                Log.i("AugmentedImage", "Image ${augmentedImage.name} tracked")
+                when (augmentedImage.name) {
 
-                        "terra.jpeg"->  tv1!!.text="terra visibile"
+                    "terra.jpeg" -> tv1!!.text = "terra visibile"
 
-                        "marte.jpeg"-> tv1!!.text="Marte visibile"
+                    "marte.jpeg" -> tv1!!.text = "Marte visibile"
 
-                        "mercurio.jpeg"->tv1!!.text="Mercurio visibile"
+                    "mercurio.jpeg" -> tv1!!.text = "Mercurio visibile"
 
-                        "rabbit.jpeg"->tv1!!.text="Rabbit visibile"
+                    "rabbit.jpeg" -> tv1!!.text = "Rabbit visibile"
 
-                    }
+                }
+                if(!modelSelected){
+                    Log.i("AugmentedImage", "Image ${augmentedImage.name} tracked")
+                    augmentedImage.createAnchor(augmentedImage.getCenterPose())
+
+
+                    val localPosition = Vector3()
+                    val centerNode = Node()
+                    val an = AnchorNode(augmentedImage.anchors.first())
+                    Log.i("AugmentedImage", augmentedImage.anchors.first().toString())
+                    Log.i("AugmentedImage", augmentedImage.anchors.size.toString())
+                    centerNode.parent = an
+                    localPosition.set(augmentedImage.getExtentX(),0.5f,augmentedImage.extentZ)
+                    //localPosition[ augmentedImage.getExtentX(), 0.5f] =  augmentedImage.getExtentZ()
+                    centerNode.localPosition = localPosition
+                    centerNode.renderable = model!!.getNow(null)
+                    arFragment.getArSceneView().getScene().addChild(centerNode)
+                    modelSelected = true
+                }
             }
-    }
 
+        }
+
+    fun setModel() {
+        if (model == null) {
+            model =
+                ModelRenderable.builder()
+                    .setSource(this, Uri.parse("models/cuboRosso.glb"))
+                    .setIsFilamentGltf(true)
+                    //.setSource(context, Uri.parse("models/borderfence-small2.sfb"))
+                    .build();
+        }
+    }
 }
